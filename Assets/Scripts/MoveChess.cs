a2a1d8d100ee4f512f50ad48464bac0c4e9797e0
shalo1040 <53967900+shalo1040@users.noreply.github.com>
2019-11-20 14:56:24 +0900

    ChessInteraction

---
+++ Assets/Scripts/MoveChess.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MoveChess : MonoBehaviour
+{
+    GameObject rook;                            //룩 생성
+
+    void Start()
+    {
+        rook = GameObject.Find("White Stone Rook");    //룩 게임 오브젝트 초기화
+    }
+
+    public void MoveForward()                           //UP 버튼 누르면 실행(버튼의 OnClick())
+    {
+        rook.transform.Translate(0, 0, 0.2f);              //위 방향으로 1만큼 이동
+    }
+
+    public void MoveBackward()                          //Down 버튼 누르면 실행
+    {
+        rook.transform.Translate(0, 0, -0.2f);             //아래 방향으로 1만큼 이동
+    }
+
+    public void MoveLeft()                              //Left 버튼 누르면 실행
+    {
+        rook.transform.Translate(-0.2f, 0, 0);             //왼쪽 방향으로 1만큼 이동
+    }
+
+    public void MoveRight()                             //Right 버튼 누르면 실행
+    {
+        rook.transform.Translate(0.2f, 0, 0);              //오른쪽 방향으로 1만큼 이동
+    }
+}
---
+++ Assets/Scripts/RotateChess.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateChess : MonoBehaviour
+{
+    Animator anim;                 //애니메이션 객체 생성
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();            //Animator 접근
+    }
+
+    public void Rotate()                            //Rotation 버튼 클릭 시 실행
+    {
+        anim.Play("RotateAnim");                      //RotateAnim 애니메이션 1회 play
+        //anim.SetInteger("Rotation", 1);             //Rotation parameter의 값을 1로 설정한다=>무한반복 됨
+    }
+}
